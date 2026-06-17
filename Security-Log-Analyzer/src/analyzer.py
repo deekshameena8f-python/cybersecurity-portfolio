b6f4ba0e6d8f9d9fe308b8ad2002d6888c3049f0
deekshameena8f-python <deekshameena8f@gmail.com>
2026-06-17 19:18:15 +0530

    Implemented failed login counting

--- Security-Log-Analyzer/src/analyzer.py
+++ Security-Log-Analyzer/src/analyzer.py
@@ -1,1 +1,10 @@
+with open("logs/sample.log") as f:
+    logs = f.readlines()
 
+failed = 0
+
+for line in logs:
+    if "LOGIN_FAILED" in line:
+        failed += 1
+
+print("Failed Logins:", failed)
