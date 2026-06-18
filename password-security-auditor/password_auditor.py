ca3120e0b787245f0702174aa17c364a9e75a017
deekshameena8f-python <deekshameena8f@gmail.com>
2026-06-18 20:54:38 +0530

    Add lowercase and numeric character detection

--- password-security-auditor/password_auditor.py
+++ password-security-auditor/password_auditor.py
@@ -3,11 +3,19 @@ password = input("Enter a password: ")
 length = len(password)
 
 has_upper = False
+has_lower = False
+has_digit = False
 
 for char in password:
     if char.isupper():
         has_upper = True
+    if char.islower():
+        has_lower = True
+     if char.isdigit():
+        has_digit = True
 
 print("Password Length:", length)
 print("Contains Uppercase:", has_upper)
+print("Contains Lowercase:", has_lower)
+print("Contains Digit:", has_digit)
 
