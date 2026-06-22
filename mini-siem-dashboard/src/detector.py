77755097d6eca6e52a282d82ea6ae999ee44d818
deekshameena8f-python <deekshameena8f@gmail.com>
2026-06-22 11:31:38 +0530

    Update detector.py

--- mini-siem-dashboard/src/detector.py
+++ mini-siem-dashboard/src/detector.py
@@ -1,1 +1,19 @@
+# src/detector.py
 
+def detect_alerts(logs):
+
+    alerts = []
+
+    for log in logs:
+
+        if log["level"] == "ERROR":
+            alerts.append(
+                f"ALERT: {log['message']}"
+            )
+
+        if "Suspicious" in log["message"]:
+            alerts.append(
+                f"ALERT: {log['message']}"
+            )
+
+    return alerts
